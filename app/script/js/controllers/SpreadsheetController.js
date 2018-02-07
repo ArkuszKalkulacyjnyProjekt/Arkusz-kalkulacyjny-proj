2d2df4df133fc6a10128227b13b41d8f3f18ca8f
Piotrek <piotrek120017@gmail.com>
2018-02-07 22:57:34 +0100

    resolving cells and calculate result, next save in db

--- app/script/js/controllers/SpreadsheetController.js
+++ app/script/js/controllers/SpreadsheetController.js
@@ -1,11 +1,44 @@
 "use strict";
 
-app.controller("spreadsheetController", ["$scope", "spreadsheetFactory",
-    function($scope, $spreadsheetFactory) {
+app.controller("spreadsheetController", ["$scope", "$parse", "spreadsheetFactory",
+    function($scope, $parse, $spreadsheetFactory) {
         $scope.spreadsheetFactory = $spreadsheetFactory;
+
+        $scope.resolveReferenceToCells = function (cellContent) {
+            cellContent = cellContent.replace("=", "");
+
+            var finalCellContent = cellContent;
+
+            var matches = cellContent.match(/[A-Z]\d+/g);
+            console.log(matches);
+            if (matches != null) {
+                for (var i = 0; i < matches.length; i++) {
+                    var referenceCellContent = $scope.spreadsheetFactory.cells[matches[i]];
+                    if (typeof referenceCellContent === 'undefined' || referenceCellContent === '') {
+                        referenceCellContent = 0;
+                    } else {
+                        referenceCellContent = $scope.resolveReferenceToCells(referenceCellContent);
+                    }
+
+                    finalCellContent = finalCellContent.replace(matches[i], referenceCellContent);
+                }
+            }
+
+            return finalCellContent;
+        };
 
         $scope.computeCell = function(cell){
             var cellContent = $scope.spreadsheetFactory.cells[cell];
+
+            if (typeof cellContent === 'undefined'){
+                return cellContent;
+            }
+
+            if (cellContent.charAt(0) == "=") {
+                var resolveFormula = $scope.resolveReferenceToCells(cellContent);
+                var result = $parse(resolveFormula)($scope);
+                return result;
+            }
             return cellContent;
         }
         
