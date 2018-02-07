d153016779c6660d934f83f21ade07906fe15415
Piotrek <piotrek120017@gmail.com>
2018-02-07 21:31:50 +0100

    add function to solve cell(now only return value insert to cell)

--- app/script/js/controllers/SpreadsheetController.js
+++ app/script/js/controllers/SpreadsheetController.js
@@ -1,8 +1,12 @@
 "use strict";
 
 app.controller("spreadsheetController", ["$scope", "spreadsheetFactory",
-    function($scope, $parse, $spreadsheetFactory) {
+    function($scope, $spreadsheetFactory) {
         $scope.spreadsheetFactory = $spreadsheetFactory;
 
+        $scope.computeCell = function(cell){
+            var cellContent = $scope.spreadsheetFactory.cells[cell];
+            return cellContent;
+        }
         
     }]);
