855385977098bd7b059d822c35883e69d5a77d0b
Piotrek <piotrek120017@gmail.com>
2018-02-10 18:20:36 +0100

    some fixes

--- app/script/js/controllers/SpreadsheetController.js
+++ app/script/js/controllers/SpreadsheetController.js
@@ -53,23 +53,21 @@ app.controller("spreadsheetController", ["$scope", "$parse", "$http", "spreadshe
                     throw new Error("Error: reference to itself");
                 for (var i = 0; i < matches.length; i++) {
 					//console.log("matche[I]" + matches[i]);
-					var cell = matches[i];
-					if (cell.charAt(0).charCodeAt() > 85 || cell.substr(1) > 20 )
+					var matchedCell = matches[i];
+					var row = parseInt(matchedCell.substr(1));
+					if ($scope.spreadsheetFactory.columns.indexOf(matchedCell.charAt(0)) < 0 || $scope.spreadsheetFactory.rows.indexOf(row) < 0 )
 						throw new Error("Error: reference to not existing cell");
                     var referenceCellContent = $scope.spreadsheetFactory.cells[matches[i]];
                     if (typeof referenceCellContent === 'undefined' || referenceCellContent === '') {
                         referenceCellContent = 0;
                     } else {
                         referenceCellContent = $scope.resolveReferenceToCells(referenceCellContent, matches[i]);
-						referenceCellContent = "(" + referenceCellContent + ")";
+						referenceCellContent =  referenceCellContent;
                     }
 
                     finalCellContent = finalCellContent.replace(matches[i], referenceCellContent);
                 }
             }
-			else{
-				throw new Error("Error: reference to not existing cell");
-			}
 			
             return finalCellContent;
         };
@@ -82,8 +80,14 @@ app.controller("spreadsheetController", ["$scope", "$parse", "$http", "spreadshe
             }		
 		
             if (cellContent.charAt(0) == "=") {
-				return calculate(cellContent, cell)
+				cellContent = $scope.calculate(cellContent, cell)
             }
+            if (cellContent === "err"){
+                $scope.turnRed()
+            }else {
+            	$scope.turnDefault()}
+
+
             return cellContent;
         };
 
@@ -99,20 +103,13 @@ app.controller("spreadsheetController", ["$scope", "$parse", "$http", "spreadshe
                 );
         };
 		
-		String.prototype.replaceAll = function(search, replace) {
-			if (replace === undefined) {
-				return this.toString();
-			}
-			return this.split(search).join(replace);
-		}
-		
-		function calculate(cellContent, cell) {
+		$scope.calculate = function(cellContent, cell) {
 			try {
 				var resolveFormula = $scope.resolveReferenceToCells(cellContent, cell);
 				console.log(resolveFormula);
 				if(resolveFormula.indexOf("+") > -1 || resolveFormula.indexOf("-") > -1 || resolveFormula.indexOf("*") > -1 || resolveFormula.indexOf("/") > -1){
 					var result = $parse(resolveFormula)($scope);
-					return (result) ? result : "err" ;
+					return result ;
 				}else {
 					return resolveFormula;
 				}
@@ -120,6 +117,13 @@ app.controller("spreadsheetController", ["$scope", "$parse", "$http", "spreadshe
 				console.log(err);
 				return "err";
 			}			
-		}
+		};
+
+        String.prototype.replaceAll = function(search, replace) {
+            if (replace === undefined) {
+                return this.toString();
+            }
+            return this.split(search).join(replace);
+        };
         
     }]);
