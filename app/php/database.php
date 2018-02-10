4ed9967a09c40c3b8ed022c49913d7932efcc9c2
Piotrek <piotrek120017@gmail.com>
2018-02-10 20:09:07 +0100

    database config info move to variables

--- app/php/database.php
+++ app/php/database.php
@@ -6,8 +6,12 @@
  * Time: 22:43
  */
 
+$host = "localhost";
+$username = "root";
+$password = "";
+$db_name = "spreadsheet_base";
 
-$conn = new mysqli("localhost", "root", "", "spreadsheet_base");
+$conn = new mysqli($host, $username, $password, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
